719906ec23a11588a32f49e29c418380865323f7
Shalvin P Shaji <shalvinpshaji.official@gmail.com>
2026-03-22 00:43:19 +0530

    Arrays and testing

---
+++ arrays/sum_test.go
@@ -0,0 +1,12 @@
+package arrays
+
+import "testing"
+
+func TestSum(t *testing.T) {
+	numbers := [5]int{1, 2, 3, 4, 5}
+	got := Sum(numbers)
+	expected := 15
+	if got != expected {
+		t.Errorf("got '%d' but expected '%d', given %v", got, expected, numbers)
+	}
+}
