2851eb2391f4d1e88332b6fb4e861f418c6063e1
Shalvin P Shaji <shalvinpshaji.official@gmail.com>
2026-03-22 00:50:07 +0530

    Use range in iteration for loop

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -6,7 +6,7 @@ const repeatCount = 5
 
 func Repeat(character string) string {
 	var repeated strings.Builder
-	for i := 0; i < repeatCount; i++ {
+	for range repeatCount {
 		repeated.WriteString(character)
 	}
 	return repeated.String()
