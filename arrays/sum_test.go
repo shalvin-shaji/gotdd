d3fb4d8e5e37b674858922f26ff7faf16867fcc4
Shalvin P Shaji <shalvinpshaji.official@gmail.com>
2026-03-22 00:46:05 +0530

    Splices golang

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -3,10 +3,20 @@ package arrays
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
-	got := Sum(numbers)
-	expected := 15
-	if got != expected {
-		t.Errorf("got '%d' but expected '%d', given %v", got, expected, numbers)
-	}
+	t.Run("collection of 5 number", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
+		got := Sum(numbers)
+		expected := 15
+		if got != expected {
+			t.Errorf("got '%d' but expected '%d', given %v", got, expected, numbers)
+		}
+	})
+	t.Run("collection of 3 number", func(t *testing.T) {
+		numbers := []int{5, 6, 7}
+		got := Sum(numbers)
+		expected := 18
+		if got != expected {
+			t.Errorf("got '%d' but expected '%d', given %v", got, expected, numbers)
+		}
+	})
 }
