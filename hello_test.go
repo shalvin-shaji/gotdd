d19789a733ec5fe31b73800ac8668a10dfd7318d
Shalvin P Shaji <shalvinpshaji.official@gmail.com>
2026-03-21 23:08:51 +0530

    New tdd module added first code and test

---
+++ hello_test.go
@@ -0,0 +1,29 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	t.Run("Saying hello to people", func(t *testing.T) {
+		got := Hello("World")
+
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("Saying 'Hello, World when empty string is passed", func(t *testing.T) {
+		got := Hello("")
+
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
